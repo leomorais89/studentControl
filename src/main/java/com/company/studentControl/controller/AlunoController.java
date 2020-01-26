97523052537a7eb14437e8f4837a97a1cdcfb35e
Leonardo Morais <leonardomorais89@yahoo.com.br>
2020-01-26 15:38:39 -0300

    Criado e testado o Controlador Rest Recuperar Alunos por Id

--- src/main/java/com/company/studentControl/controller/AlunoController.java
+++ src/main/java/com/company/studentControl/controller/AlunoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,12 @@ public class AlunoController {
 	public ResponseEntity<List<Aluno>> findAll(){
 		List<Aluno> alunos = service.findAll();
 		return ResponseEntity.ok().body(alunos);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Aluno> findAll(@PathVariable Integer id){
+		Aluno aluno = service.findById(id);
+		return ResponseEntity.ok().body(aluno);
 	}
 
 }
