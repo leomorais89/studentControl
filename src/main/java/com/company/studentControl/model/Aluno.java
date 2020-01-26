fd0b31ab0c796947a28594a20479af20f2ad11c7
Leonardo Morais <leonardomorais89@yahoo.com.br>
2020-01-26 14:59:06 -0300

    Criado o modelo de entidade JPA Aluno

---
+++ src/main/java/com/company/studentControl/model/Aluno.java
@@ -0,0 +1,55 @@
+package com.company.studentControl.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Aluno implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private Integer idade;
+	
+	public Aluno() {
+		
+	}
+
+	public Aluno(Integer id, String nome, Integer idade) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.idade = idade;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+}
