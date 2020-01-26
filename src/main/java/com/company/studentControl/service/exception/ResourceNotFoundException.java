10e83cbe1664f49f49d3375f8e86bf867042ea28
Leonardo Morais <leonardomorais89@yahoo.com.br>
2020-01-26 16:02:03 -0300

    Sistema finalizado

--- src/main/java/com/company/studentControl/service/exception/ResourceNotFoundException.java
+++ src/main/java/com/company/studentControl/service/exception/ResourceNotFoundException.java
@@ -3,6 +3,8 @@ package com.company.studentControl.service.exception;
 public class ResourceNotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 	
+	//Classe para tratar exceção
+	
 	public ResourceNotFoundException(String msg) {
 		super(msg);
 	}
