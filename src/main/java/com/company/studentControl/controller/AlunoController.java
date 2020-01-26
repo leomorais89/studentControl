98e65999015287ef3c96929924fc5888fd82e999
Leonardo Morais <leonardomorais89@yahoo.com.br>
2020-01-26 15:49:23 -0300

    Criado e testado o Controlador Rest Deletar Aluno, Sistema básico finalizado

--- src/main/java/com/company/studentControl/controller/AlunoController.java
+++ src/main/java/com/company/studentControl/controller/AlunoController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,6 +50,12 @@ public class AlunoController {
 	public ResponseEntity<Aluno> update(@PathVariable Integer id, @Valid @RequestBody Aluno aluno) {
 		aluno = service.update(id, aluno);
 		return ResponseEntity.ok().body(aluno);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deleteById(@PathVariable Integer id) {
+		service.deleteById(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
