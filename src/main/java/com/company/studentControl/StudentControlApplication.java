1c3044335945966bea5d3b97c142e26be1037e2b
Leonardo Morais <leonardomorais89@yahoo.com.br>
2020-01-26 14:39:51 -0300

    API Criada

---
+++ src/main/java/com/company/studentControl/StudentControlApplication.java
@@ -0,0 +1,13 @@
+package com.company.studentControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/studentControl/StudentControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.studentControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
